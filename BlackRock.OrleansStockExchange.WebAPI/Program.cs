5f54380ce04d8e4120ffbea79eed5e9bbe7be910
Dimitrije Veljkovic <Dimitrije.Veljkovic@blackrock.com>
2022-10-04 13:09:10 +0200

    Local

--- BlackRock.OrleansStockExchange.WebAPI/Program.cs
+++ BlackRock.OrleansStockExchange.WebAPI/Program.cs
@@ -13,36 +13,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseOrleans(orleansBuilder =>
 {
     orleansBuilder
-        //.UseLocalhostClustering()
-        //.AddSimpleMessageStreamProvider(StorageConstants.TransactionsStreamName, o => o.FireAndForgetDelivery = true)
-        //.AddMemoryGrainStorage("PubSubStore")
-        .UseKubernetesHosting()
-        .AddAzureTableGrainStorage(
-            "PubSubStore",
-            options => options.ConfigureTableServiceClient(builder.Configuration["StorageAccount:ConnectionString"]))
-        .AddEventHubStreams(StorageConstants.MainBoardStreamName, (ISiloEventHubStreamConfigurator configurator) =>
-        {
-            configurator.ConfigureEventHub(eventHub => eventHub.Configure(options =>
-            {
-                options.ConfigureEventHubConnection(
-                    builder.Configuration["EventHub:ConnectionString"],
-                    "streamhub",
-                    "$Default");
-            }));
-            configurator.UseAzureTableCheckpointer(
-                tableBulder => tableBulder.Configure(options =>
-                {
-                    options.ConfigureTableServiceClient(builder.Configuration["StorageAccount:ConnectionString"]);
-                    options.PersistInterval = TimeSpan.FromSeconds(10);
-                }));
-        })
-        .AddAzureTableGrainStorage(
-            StorageConstants.BlobStorageName,
+        .UseLocalhostClustering()
+        .AddSimpleMessageStreamProvider(StorageConstants.MainBoardStreamName, o => o.FireAndForgetDelivery = true)
+        .AddMemoryGrainStorage("PubSubStore")
+        .AddMemoryGrainStorage(StorageConstants.BlobStorageName);
+        //.UseKubernetesHosting()
+        //.AddAzureTableGrainStorage(
+        //    "PubSubStore",
+        //    options => options.ConfigureTableServiceClient(builder.Configuration["StorageAccount:ConnectionString"]))
+        //.AddEventHubStreams(StorageConstants.MainBoardStreamName, (ISiloEventHubStreamConfigurator configurator) =>
+        //{
+        //    configurator.ConfigureEventHub(eventHub => eventHub.Configure(options =>
+        //    {
+        //        options.ConfigureEventHubConnection(
+        //            builder.Configuration["EventHub:ConnectionString"],
+        //            "streamhub",
+        //            "$Default");
+        //    }));
+        //    configurator.UseAzureTableCheckpointer(
+        //        tableBulder => tableBulder.Configure(options =>
+        //        {
+        //            options.ConfigureTableServiceClient(builder.Configuration["StorageAccount:ConnectionString"]);
+        //            options.PersistInterval = TimeSpan.FromSeconds(10);
+        //        }));
+        //})
+        //.AddAzureTableGrainStorage(
+        //    StorageConstants.BlobStorageName,
 
-            options => options.ConfigureTableServiceClient(builder.Configuration["StorageAccount:ConnectionString"]));
+    //    options => options.ConfigureTableServiceClient(builder.Configuration["StorageAccount:ConnectionString"]));
 
-    orleansBuilder.UseRedisClustering(options => options.ConnectionString = "redis:6379");
-    orleansBuilder.AddRedisGrainStorage("definitions", options => options.ConnectionString = "redis:6379");
+    //orleansBuilder.UseRedisClustering(options => options.ConnectionString = "redis:6379");
+    //orleansBuilder.AddRedisGrainStorage("definitions", options => options.ConnectionString = "redis:6379");
 
     orleansBuilder.UseDashboard();
 });
